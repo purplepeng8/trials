3f09cb1485ac05ee1340a57b4549a39513dfdec3
eitan gov-ary <eitan@eitans-MacBook-Pro.local>
2016-10-06 11:15:05 +0300

    File

---
+++ run.py
@@ -0,0 +1,20 @@
+import unittest
+import logging
+import sys
+
+
+def asserti(x,y):
+    if x == y :
+        print 'OK'
+        #raise ValueError('represents a hidden bug, do not catch this')
+    else :
+        raise ValueError('mismatch')
+
+a=1
+b=2
+
+print 'Hi'
+asserti(a,1)
+asserti(b,2)
+print 'Bye'
+
