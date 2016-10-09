012861f2fc049663143912f6e9ea4f2654dc3715
Eitan <eitan@3dsig.com>
2016-10-09 11:19:35 +0300

    hmmm

--- run.py
+++ run.py
@@ -9,12 +9,13 @@ def asserti(x,y):
         #raise ValueError('represents a hidden bug, do not catch this')
     else :
         raise ValueError('mismatch')
+        sys.exit(1)
 
 a=1
 b=2
 
 print 'Hi'
 asserti(a,1)
-asserti(b,4)
+asserti(b,2)
 print 'Bye'
 
